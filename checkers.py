090893a99d0a98ca99a3165b53053a5f7ac30728
reilly-callaway <reilly.callaway@gmail.com>
2015-02-28 12:20:57 +1100

    MultiJump move is working but need to add some error handling for when checking if a jump move is possible in the case of checking a position out of range of the board

--- checkers.py
+++ checkers.py
@@ -1,30 +1,6 @@
 import os
 
-def MultiJump(player, PieceX, PieceY):
-	# Piece position = moved position.
-	#Already did this before start of function!
-													
-	# check if new jump is possible --> if not return(turns over)
-	if(True):#Jump move possible
-		
-		# get input again for new move position
-		MoveX, MoveY = input('Move?(x,y): ').split(',') #Co-ordinates of where you want to move it to
-		MoveX, MoveY = int(MoveX)-1, int(MoveY)-1
-		# check if jump move
-		if (MoveX == PieceX + 2 or MoveX == PieceX - 2) and (MoveY == PieceY + 2 or MoveY == PieceY - 2) and (board[MoveY][MoveX] == '-') and (board[int(PieceY+((MoveY-PieceY)/2))][int(PieceX+((MoveX-PieceX)/2))] != '-' and board[int(PieceY+((MoveY-PieceY)/2))][int(PieceX+((MoveX-PieceX)/2))] != player):
-			# If jump --> Make jump then repeat
-			board[PieceY][PieceX] = '-'	
-			board[MoveY][MoveX] = player 				#Jump move
-			board[int(PieceY+((MoveY-PieceY)/2))][int(PieceX+((MoveX-PieceX)/2))] = '-' #Need to configure this so you have option 
-			Score[player] += 1															#to make another jump move if possible
-			PieceX, PieceY = MoveX, MoveY
-			MultiJump(player, PieceX, PieceY)
-		else:
-			print('INVALID MOVE!!!')
-			MultiJump(player, PieceX, PieceY)
-		# If not --> Invalid move --> get input again. 
-	else:
-		return
+
 def PlayerTurn(player):
 	print('Player {}\'s turn:'.format(player))
 	PieceX, PieceY = input('Piece?(x,y): ').split(',')
@@ -42,13 +18,46 @@ def PlayerTurn(player):
 			board[int(PieceY+((MoveY-PieceY)/2))][int(PieceX+((MoveX-PieceX)/2))] = '-' #Need to configure this so you have option 
 			Score[player] += 1															#to make another jump move if possible
 			PieceX, PieceY = MoveX, MoveY
+			PrintBoard()
 			MultiJump(player, PieceX, PieceY)
+			return
 		else:
 			print('INVALID MOVE!!!')
 			PlayerTurn(player)
 	else:
 		print('INVALID PIECE!!!')
 		PlayerTurn(player)
+
+def JumpMoveCheck(player, PieceY, PieceX):
+	for Yadd in (1, -1):
+		for Xadd in (1, -1):
+			if(board[PieceY + Yadd][PieceX + Xadd] != '-' and board[PieceY + Yadd][PieceX + Xadd] != player and board[PieceY + (2*Yadd)][PieceX + (2*Xadd)] == '-'): return True;
+	return False
+def MultiJump(player, PieceX, PieceY):
+	# Piece position = moved position.
+	#Already did this before start of function!
+
+	# check if new jump is possible --> if not return(turns over)
+	if(JumpMoveCheck(player, PieceY, PieceX)):#Jump move possible
+		# get input again for new move position
+		MoveX, MoveY = input('Move?(x,y): ').split(',') #Co-ordinates of where you want to move it to
+		MoveX, MoveY = int(MoveX)-1, int(MoveY)-1
+		# check if jump move
+		if (MoveX == PieceX + 2 or MoveX == PieceX - 2) and (MoveY == PieceY + 2 or MoveY == PieceY - 2) and (board[MoveY][MoveX] == '-') and (board[int(PieceY+((MoveY-PieceY)/2))][int(PieceX+((MoveX-PieceX)/2))] != '-' and board[int(PieceY+((MoveY-PieceY)/2))][int(PieceX+((MoveX-PieceX)/2))] != player):
+			# If jump --> Make jump then repeat
+			board[PieceY][PieceX] = '-'	
+			board[MoveY][MoveX] = player 				#Jump move
+			board[int(PieceY+((MoveY-PieceY)/2))][int(PieceX+((MoveX-PieceX)/2))] = '-' #Need to configure this so you have option 
+			Score[player] += 1															#to make another jump move if possible
+			PieceX, PieceY = MoveX, MoveY
+			PrintBoard()
+			MultiJump(player, PieceX, PieceY)
+		else:
+			print('INVALID MOVE!!!')
+			MultiJump(player, PieceX, PieceY)
+		# If not --> Invalid move --> get input again. 
+	else:
+		return
 
 def PrintBoard():
 	print('Player1(X): {} Player2(O): {}'.format(Score[Player1], Score[Player2]))
@@ -67,23 +76,23 @@ Score = {Player1:0, Player2:0}
 board = [
 ['X', '*', 'X', '*', 'X', '*', 'X', '*'],
 ['*', 'X', '*', 'X', '*', 'X', '*', 'X'], 
-['X', '*', 'X', '*', 'X', '*', 'X', '*'], 
-['*', '-', '*', '-', '*', '-', '*', '-'], 
-['-', '*', '-', '*', '-', '*', '-', '*'],
-['*', 'O', '*', 'O', '*', 'O', '*', 'O'], 
+['-', '*', '-', '*', 'X', '*', '-', '*'], 
+['*', 'X', '*', 'X', '*', 'X', '*', '-'], 
+['O', '*', '-', '*', '-', '*', '-', '*'],
+['*', '-', '*', 'O', '*', 'O', '*', 'O'], 
 ['O', '*', 'O', '*', 'O', '*', 'O', '*'],
 ['*', 'O', '*', 'O', '*', 'O', '*', 'O']]
 
-
+os.system('CLS')					#Remove this also if using the IDLE
 while (True):
 	PrintBoard()
 	PlayerTurn(Player1)
 	os.system('CLS')				#Remove this is using the IDLE, if using terminal/command line/cmd/whatever else you kids call it nowerdays:
-	if Score[Player1] >= 2: break;	#It will clear the terminal of all text so it doesn't look like reprinting the board over and over. Just one board :)
+	if Score[Player1] >= 4: break;	#It will clear the terminal of all text so it doesn't look like reprinting the board over and over. Just one board :)
 	PrintBoard()			
 	PlayerTurn(Player2)
 	os.system('CLS')				#Remove this one too if using IDLE, I haven't tested it but it will probably break it :(
-	if Score[Player2] >= 2: break;
+	if Score[Player2] >= 4: break;
 
 PrintBoard()
 print("Player 1 (X) WINS!!!" if Score[Player1] >= Score[Player2] else "Player 2 (O) WINS!!!")
