97bfefbcbfb91df23eea76e2b84b36ad94b76510
reilly-callaway <reilly.callaway@gmail.com>
2015-03-03 21:35:00 +1100

    Main outline of menu complete, spacing all figured out although math could probably be simplified down

--- Menu.py
+++ Menu.py
@@ -8,19 +8,34 @@ ArrowPosition = 0
 # Left '\xe0' 'K' = 224 75
 # Right '\xe0' 'M' = 224 77
 # Enter '\r' = 13
+# ESC ...   = 27
+
 key = 0
+
+options = ["Play", "Rules", "Settings", "sWag3KawL5u"]
 
 while key != 27:
 	key = ord(getch())
 	system('CLS')
 	if key == 13:
-		print("Option {} Selected".format(ArrowPosition))
+		print("Option \"{}\" Selected".format(options[ArrowPosition-1]))
 	elif key == 224:
 		key = ord(getch())
 		if key == 75 and ArrowPosition > 1:
 			ArrowPosition -= 1
-		elif key == 77 and ArrowPosition < 10:
+		elif key == 77 and ArrowPosition < 4:
 			ArrowPosition += 1
+	Space = 15
+
+	# for i in options:
+	#  	if len(i) > Space + 4: Space = len(i) + 4
+
+	for m in range(20):
+		print("This is filler text", end=" ")
+	print()
 	
-	print('01234567890')
-	print(" " * (ArrowPosition - 1), "^", '\n', end="")
+	for n in options:
+		print(" "*(Space-(len(n))), n, end="")
+	print()
+
+	print(" " * ((ArrowPosition * (Space+1)) - len(options[ArrowPosition-1]) - 1), "^", '\n', end="")
